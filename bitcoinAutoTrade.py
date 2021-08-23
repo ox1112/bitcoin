ef382fabe8cd2099316c1e05bc78b0557a52d6f9
ox1112 <89411429+ox1112@users.noreply.github.com>
2021-08-24 02:21:22 +0900

    Update bitcoinAutoTrade.py
    [윤상수] 계정 연동

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -2,8 +2,8 @@ import time
 import pyupbit
 import datetime
 
-access = "your-access"
-secret = "your-secret"
+access = "qiR0RblT7nwNKnAxdEdXGE8qzJnHBxwCNOhao7aT"
+secret = "G3WEpxUD61uopT0dcdhKetlRQdrYSLFxxPwciOEt"
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
